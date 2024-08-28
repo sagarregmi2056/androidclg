63abed6e5458be922cf582809409c4504d146d42
sagar regmi <115308062+sagarregmi2056@users.noreply.github.com>
2024-08-28 09:09:18 +0545

    complete register api

--- app/src/main/java/com/example/wastepickerapp/Api.java
+++ app/src/main/java/com/example/wastepickerapp/Api.java
@@ -3,11 +3,13 @@ package com.example.wastepickerapp;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface Api {
-    @POST("Register")
-    Call<ResponseBody> RegisterUser(
+    @FormUrlEncoded
+    @POST("register")
+    Call<ResponseBody> Register(
             @Field("username") String username,
 
             @Field("email")String email,
--- app/src/main/java/com/example/wastepickerapp/SignupActivity.java
+++ app/src/main/java/com/example/wastepickerapp/SignupActivity.java
@@ -6,12 +6,20 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SignupActivity extends AppCompatActivity {
     private Button login_BTN ,register_BTN;
@@ -38,10 +46,10 @@ register_BTN.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View view) {
         String email = editTextEmail.getText().toString().trim();
-        String name = editTextUsername.getText().toString().trim();
+        String username = editTextUsername.getText().toString().trim();
 
         String password = editTextPassword.getText().toString().trim();
-        String confirmpassword = editTextConfirmPw.getText().toString().trim();
+        String confirm_password = editTextConfirmPw.getText().toString().trim();
         if (email.isEmpty()) {
             editTextEmail.setError("email is required");
             editTextEmail.requestFocus();
@@ -68,16 +76,35 @@ register_BTN.setOnClickListener(new View.OnClickListener() {
             return;
         }
 
-        if (confirmpassword.isEmpty()) {
+        if (confirm_password.isEmpty()) {
             editTextConfirmPw.setError("Confirm Password is required");
             editTextConfirmPw.requestFocus();
             return;
         }
-        if (!password.equals(confirmpassword)) {
+        if (!password.equals(confirm_password)) {
             editTextConfirmPw.setError("Passwords do not match");
             editTextConfirmPw.requestFocus();
             return;
         }
+        Call<ResponseBody> call = RetrofitClient.getInstance().getApi().Register(username,email,password,confirm_password);
+
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+            try {
+                String s = response.body().string();
+                Toast.makeText(SignupActivity.this,s, Toast.LENGTH_SHORT).show();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable throwable) {
+                Toast.makeText(SignupActivity.this, throwable.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 });
